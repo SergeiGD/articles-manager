079fe40ff11668c84963ee91df820e5b9622cd9f
sergei <sg_16072002@mail.ru>
2023-04-24 21:14:51 +0800

    models

--- articlesmanager/articles/models.py
+++ articlesmanager/articles/models.py
@@ -2,16 +2,19 @@ from django.db import models
 from django.utils import timezone
 from authors.models import Author
 from states.models import State
+from users.models import CustomUser
 
 
 class ArticleState(models.Model):
     article = models.ForeignKey(
         to='Article',
         on_delete=models.CASCADE,
+        verbose_name='Статья',
     )
     state = models.ForeignKey(
         to=State,
         on_delete=models.CASCADE,
+        verbose_name='Статус',
     )
     date_set = models.DateTimeField(default=timezone.now, verbose_name='Дата присвоения')
 
@@ -35,8 +38,12 @@ class Article(models.Model):
         related_name='articles',
         related_query_name='article'
     )
+    users = models.ManyToManyField(
+        verbose_name='Проверяющие',
+        related_name='users',
+        related_query_name='user',
+        to=CustomUser,
+    )
     date_created = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
     date_edited = models.DateTimeField(null=True, blank=True, verbose_name='Дата изменения')
     date_deleted = models.DateTimeField(null=True, blank=True, verbose_name='Дата удаления')
-
-
--- articlesmanager/reviews/models.py
+++ articlesmanager/reviews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from users.models import CustomUser
+from articles.models import Article
 
-# Create your models here.
+
+class Review(models.Model):
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+    approved = models.BooleanField(verbose_name='Одобрено', default=False)
+    user = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.PROTECT,
+        verbose_name='Проверяющий',
+        related_name='reviews',
+        related_query_name='reviewss',
+    )
+    article = models.ForeignKey(
+        to=Article,
+        on_delete=models.CASCADE,
+        verbose_name='Статья',
+        related_name='reviews',
+        related_query_name='review',
+    )
+    date_created = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
