4f3dec2ad7a5f040b9936cfbe881972795974068
sergei <sg_16072002@mail.ru>
2023-06-22 12:42:10 +0800

    apache

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+echo "Waiting for postgres..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
+apachectl -D FOREGROUND
+
+exec "$@"
